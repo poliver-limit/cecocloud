3089f299f9aaf2e1f6e7e92a75d1be300f8fe40a
josepg-limit <josepg@limit.es>
2019-11-18 17:43:57 +0100

    Identificador: generació de codi automàtic

--- cecocloud-module/cecocloud-module-facturacio/cecocloud-module-facturacio-back/src/main/java/es/limit/cecocloud/facturacio/back/controller/ZonaApiController.java
+++ cecocloud-module/cecocloud-module-facturacio/cecocloud-module-facturacio-back/src/main/java/es/limit/cecocloud/facturacio/back/controller/ZonaApiController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import es.limit.base.boot.back.controller.AbstractIdentificableApiController;
 import es.limit.base.boot.back.controller.ApiControllerHelper;
+import es.limit.base.boot.logic.api.dto.util.GenericReference;
 import es.limit.cecocloud.facturacio.logic.api.dto.Zona;
 import es.limit.cecocloud.facturacio.logic.api.service.ZonaService;
+import es.limit.cecocloud.logic.api.dto.Empresa;
+import es.limit.cecocloud.logic.api.dto.UserSession;
+import es.limit.cecocloud.logic.api.service.EmpresaService;
 
 /**
  * Controlador per al servei REST de gestió relacions perfil-rol.
@@ -23,10 +27,21 @@ public class ZonaApiController extends AbstractIdentificableApiController<Zona, 
 
 	@Autowired
 	private ZonaService service;
+	@Autowired
+	private EmpresaService empresaService;
 
 	@Override
 	protected ZonaService getService() {
 		return service;
 	}
+
+	@Override
+	protected void completeDtoWithSession(Zona dto, Object userSession) {
+		Long empresaId = ((UserSession)userSession).getEmpresa();
+		Empresa empresa = empresaService.getOne(empresaId);
+		dto.setIdentificador(GenericReference.toGenericReference(empresa.getIdentificador().getId()));
+	}
+	
+	
 
 }
