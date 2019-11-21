8acee716e318b3b71439aa78fa1858547bdbaeaa
Josep Gayà <josepg@limit.es>
2019-11-21 10:21:56 +0100

    Revisió de la selecció companyia / empresa

--- cecocloud-front/src/main/resources/src/app/modules/marc/marc.module.ts
+++ cecocloud-front/src/main/resources/src/app/modules/marc/marc.module.ts
@@ -3,24 +3,30 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BngAuthGuard } from 'base-angular';
 
+import { SelectedEmpresaGuard } from '../../shared/selected-empresa.guard';
+
 @NgModule({
 	imports: [
 		CommonModule,
 		RouterModule.forChild([{
 			path: '',
-			loadChildren: './pages/index/index.module#IndexModule',
-			canActivate: [BngAuthGuard]
-        },{
-			path: 'operaris',
-			loadChildren: './pages/operaris/operaris.module#OperarisModule',
-			canActivate: [BngAuthGuard]
-		}, {
-			path: 'marcatges',
-			loadChildren: './pages/marcatges/marcatges.module#MarcatgesModule',
-			canActivate: [BngAuthGuard]
-		}, {
-			path: '**',
-			redirectTo: ''
+			canActivate: [SelectedEmpresaGuard],
+			children: [{
+				path: '',
+				loadChildren: './pages/index/index.module#IndexModule',
+				canActivate: [BngAuthGuard]
+	        },{
+				path: 'operaris',
+				loadChildren: './pages/operaris/operaris.module#OperarisModule',
+				canActivate: [BngAuthGuard]
+			}, {
+				path: 'marcatges',
+				loadChildren: './pages/marcatges/marcatges.module#MarcatgesModule',
+				canActivate: [BngAuthGuard]
+			}, {
+				path: '**',
+				redirectTo: ''
+			}]
 		}])
     ]
 })
