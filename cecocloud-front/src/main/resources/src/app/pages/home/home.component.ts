8c4968af98ff5b677cd80d2615b798563cb80805
Josep Gayà <josepg@limit.es>
2019-12-02 10:53:50 +0100

    Nou tema

--- cecocloud-front/src/main/resources/src/app/pages/home/home.component.ts
+++ cecocloud-front/src/main/resources/src/app/pages/home/home.component.ts
@@ -8,6 +8,14 @@ import { MenuService, AppMenuItem } from '../../shared/menu.service';
 <div *ngIf="!mobileScreen" class="mat-display-3 centered" style="text-align: center">
 	<p style="margin:0"><mat-icon inline="true" style="font-size:100px;">cloud_queue</mat-icon></p>
     <p style="margin:.4em">{{'home.salutacio'|translate}}</p>
+	<!--div class="example-button-row">
+		<button mat-raised-button>Basic</button>&nbsp;
+		<button mat-raised-button color="primary">Primary</button>&nbsp;
+		<button mat-raised-button color="accent">Accent</button>&nbsp;
+		<button mat-raised-button color="warn">Warn</button>&nbsp;
+		<button mat-raised-button disabled>Disabled</button>&nbsp;
+		<a mat-raised-button routerLink=".">Link</a>
+	</div-->
 </div>
 <ng-container *ngIf="mobileScreen">
     <p *ngFor="let item of allowedMenuItems" style="text-align:center">
