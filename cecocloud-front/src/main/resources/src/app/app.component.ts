362fb8ff71d16eaab458a64e3a30219577c7d656
Josep Gayà <josepg@limit.es>
2020-01-30 08:05:40 +0100

    No apareixia el títol de l'aplicació correcte

--- cecocloud-front/src/main/resources/src/app/app.component.ts
+++ cecocloud-front/src/main/resources/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { AppService } from './shared/app.service';
 	selector: 'app-root',
 	template: `
 <bng-base-app
-	title="Cecocloud"
-	icon="cloud_queue"
+	appTitle="Cecocloud"
+	appIcon="cloud_queue"
 	(moduleSelected)="onModuleSelected($event)">
 	<button mat-icon-button *ngIf="tokenPayload?.rol.includes('ADMIN')" (click)="onAdminButtonClick()" style="margin-right:.5em">
 		<mat-icon>build</mat-icon>
