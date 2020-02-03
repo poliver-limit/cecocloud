e243843cb78e8cdce4061404bdbf9ab49eb1318c
Josep Gayà <josepg@limit.es>
2020-02-03 12:28:59 +0100

    Nou mòdul de licitacions

---
+++ cecocloud-module/cecocloud-module-lici/cecocloud-module-lici-logic-api/src/main/java/es/limit/cecocloud/lici/logic/api/dto/LicitacioConfig.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package es.limit.cecocloud.lici.logic.api.dto;
+
+import javax.persistence.Transient;
+
+import es.limit.base.boot.logic.api.annotation.RestapiField;
+import es.limit.base.boot.logic.api.dto.ProfileResourceField.RestapiFieldType;
+import es.limit.base.boot.logic.api.dto.util.AbstractIdentificable;
+import es.limit.base.boot.logic.api.dto.util.GenericReference;
+import es.limit.cecocloud.logic.api.dto.Empresa;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Informació d'una configuració del mòdul de licitacions per a una empresa.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+@Getter @Setter
+public class LicitacioConfig extends AbstractIdentificable<Long> {
+
+	private boolean sincronitzacioActiva;
+	private String filtreProvincia;
+	private String filtreCpv;
+	@Transient
+	@RestapiField(
+			type = RestapiFieldType.LOV,
+			includeInQuickFilter = false)
+	private GenericReference<Empresa, Long> empresa;
+
+}
