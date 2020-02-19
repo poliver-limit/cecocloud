fd1fabb4b5bc160b01342982904c4b4e264c14a4
mariag-limit <mariag@limit.es>
2020-02-19 13:07:16 +0100

    --LicitacionsGridComponent método onRowClicked(event: any)

--- cecocloud-module/cecocloud-module-lici/cecocloud-module-lici-front/src/main/resources/src/app/modules/lici/pages/licitacions/licitacions-grid.component.ts
+++ cecocloud-module/cecocloud-module-lici/cecocloud-module-lici-front/src/main/resources/src/app/modules/lici/pages/licitacions/licitacions-grid.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { LicitacionsService } from './licitacions.service';
 
 @Component( {
     template: `
     <bng-datagrid
-        bng-datagrid-mant
+        #datagrid
         [config]="datagridConfig"
-        [restapiService]="licitacionsService"></bng-datagrid>`
+        [restapiService]="licitacionsService"
+        (rowClicked)="onRowClicked($event)"></bng-datagrid>`
 } )
 export class LicitacionsGridComponent implements OnInit {
 
@@ -19,7 +21,13 @@ export class LicitacionsGridComponent implements OnInit {
     ngOnInit() {
     }
 
+    onRowClicked(event: any) {
+		this.router.navigate(['update', event.data.id], { relativeTo: this.route });
+	}
+
     constructor(
+        private router: Router,
+        private route: ActivatedRoute,
         public licitacionsService: LicitacionsService ) { }
 
 }
