3f323d2777dfb781a2edee09826d24c90c97f0d3
Josep Gayà <josepg@limit.es>
2020-02-27 07:58:45 +0100

    CEC-600

--- cecocloud-main/src/main/resources/data-oracle.sql
+++ cecocloud-main/src/main/resources/data-oracle.sql
@@ -4,3 +4,22 @@ INSERT INTO usuari_authority
 VALUES(hibernate_sequence.currval,'ADMIN');
 INSERT INTO usuari(id, codi, contrasenya, email, imatge_url, nom, llinatges, validat, actiu, created_by, created_date, version)
 VALUES(hibernate_sequence.nextval,'test','$2a$10$DOpxYy8VGZcKnW6aA9y4uO17KpIMFCpZRqlYjHFlFo5R/pM354g52','test@limit.es',NULL,'Test','Test',1,1,'init',current_timestamp,0);
+
+--
+-- Importar IDFs
+--
+/*
+insert into identificador(
+id, codi, descripcio, propietari_id, llicencia, llicencia_ok, 
+data_inici, data_fi, num_empreses, num_usuaris, num_operaris, created_by, created_date, version)
+select 
+    hibernate_sequence.nextval,
+    idf_cod, idf_nom,
+    1, '123', 1, sysdate, sysdate + 10000, 10, 10, 10,
+    'admin', idf_datcre, 0
+from tges_idf;
+*/
+
+--
+-- Associar totes les funcionalitats amb un IDF
+--
