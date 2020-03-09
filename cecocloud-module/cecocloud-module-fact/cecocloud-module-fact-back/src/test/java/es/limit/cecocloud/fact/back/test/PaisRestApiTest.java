908a440f2b65ad08fdbccef023661bfe0f204453
Josep Gayà <josepg@limit.es>
2020-03-09 12:58:26 +0100

    Tests pels recursos dels mòduls

--- cecocloud-module/cecocloud-module-fact/cecocloud-module-fact-back/src/test/java/es/limit/cecocloud/fact/back/test/PaisRestApiTest.java
+++ cecocloud-module/cecocloud-module-fact/cecocloud-module-fact-back/src/test/java/es/limit/cecocloud/fact/back/test/PaisRestApiTest.java
@@ -6,17 +6,21 @@ package es.limit.cecocloud.fact.back.test;
 import org.junit.Test;
 import org.springframework.security.test.context.support.WithMockUser;
 
-import es.limit.base.boot.test.AbstractRestApiTest;
 import es.limit.base.boot.test.CrudTester;
 import es.limit.cecocloud.fact.back.test.tester.PaisCrudTester;
 import es.limit.cecocloud.fact.logic.api.dto.Pais;
+import es.limit.cecocloud.logic.api.module.FuncionalitatCodiFont;
+import es.limit.cecocloud.logic.api.module.Modul;
+import es.limit.cecocloud.logic.api.module.ModuleInfo;
+import es.limit.cecocloud.logic.api.module.Modules;
+import es.limit.cecocloud.test.ModuleResourceRestApiTest;
 
 /**
  * Test pels objectes de tipus pais.
  * 
  * @author Limit Tecnologies <limit@limit.es>
  */
-public class PaisRestApiTest extends AbstractRestApiTest<Pais, String> {
+public class PaisRestApiTest extends ModuleResourceRestApiTest<Pais> {
 
 	@Override
 	protected CrudTester<Pais> getCrudTester() {
@@ -27,6 +31,12 @@ public class PaisRestApiTest extends AbstractRestApiTest<Pais, String> {
 	@Test
 	public void crudTest() {
 		genericCrudTest();
+	}
+
+	@Override
+	protected FuncionalitatCodiFont getFuncionalitatCodiFont() {
+		ModuleInfo moduleInfo = (ModuleInfo)Modules.registeredGetOne(Modul.fact.name()).get();
+		return moduleInfo.getFuncionalitats().get("FAC_PAIS");
 	}
 
 }
