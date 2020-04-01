953d00480b0e923c29efb56674c8438f20eded02
Josep Gayà <josepg@limit.es>
2020-04-01 13:40:49 +0200

    Servei de generació del profile

---
+++ cecocloud-logic/src/main/java/es/limit/cecocloud/logic/service/ProfileServiceImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package es.limit.cecocloud.logic.service;
+
+import org.springframework.stereotype.Service;
+
+import es.limit.base.boot.logic.api.dto.Profile;
+import es.limit.base.boot.logic.service.AbstractProfileServiceImpl;
+
+/**
+ * Implementació del servei per a obtenir el perfil d'un recurs.
+ * 
+ * @author Limit Tecnologies
+ */
+@Service
+public class ProfileServiceImpl extends AbstractProfileServiceImpl {
+
+	@Override
+	protected void processGeneratedProfile(Class<?> resourceClass, Profile profile) {
+	}
+
+}
