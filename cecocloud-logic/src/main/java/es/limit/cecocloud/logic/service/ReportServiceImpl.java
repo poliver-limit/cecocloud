8277736422f23bc463954b3e7490e1bcb46b44b1
Josep Gayà <josepg@limit.es>
2020-04-03 13:54:29 +0200

    Primera versió reports

---
+++ cecocloud-logic/src/main/java/es/limit/cecocloud/logic/service/ReportServiceImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package es.limit.cecocloud.logic.service;
+
+import java.net.URL;
+
+import org.springframework.stereotype.Service;
+
+import es.limit.base.boot.logic.service.AbstractReportServiceImpl;
+
+/**
+ * Implementació del servei encarregat de la generació d'informes.
+ * 
+ * @author Limit Tecnologies
+ */
+@Service
+public class ReportServiceImpl extends AbstractReportServiceImpl {
+
+	@Override
+	protected URL getReportResourceUrl(String code) {
+		return null;
+	}
+
+}
