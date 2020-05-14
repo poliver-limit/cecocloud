2f811fdec39a8604e9fc30710925bfe3011cf7e9
Josep Gayà <josepg@limit.es>
2020-05-14 12:25:22 +0200

    API Keys

--- cecocloud-front/src/main/resources/src/app/pages/apikeys/apikeys-form.component.ts
+++ cecocloud-front/src/main/resources/src/app/pages/apikeys/apikeys-form.component.ts
@@ -9,11 +9,31 @@ import { ApikeysService } from './apikeys.service';
 <bng-form
 	bng-form-mant
 	[config]="formConfig"
-	[restapiService]="apikeysService">
+	[restapiService]="apikeysService"
+	(resourceSave)="onFormResourceSave($event)">
+	<mat-card *ngIf="showKey" class="background-accent">
+		<mat-card-content>
+			<p>Assegura't de fer una còpia d'aquesta clau. Quan tanquis aquest formulari no podràs consultar-la de nou.</p>
+			<pre>{{key}}</pre>
+		</mat-card-content>
+	</mat-card>
 </bng-form>
 `
 })
 export class ApikeysFormComponent extends BngFormBaseComponent {
+
+	showKey: boolean;
+	key: boolean;
+
+	onFormResourceSave(apiKey: any) {
+		console.log('>>> apiKey', apiKey.resource.key)
+		if (apiKey.resource.key) {
+			this.key = apiKey.resource.key;
+			this.showKey = true;
+		} else {
+			this.showKey = false;
+		}
+	}
 
 	constructor(
 		activatedRoute: ActivatedRoute,
