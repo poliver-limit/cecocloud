f8c080cf32258c2d5e35e321afc5916f7f707491
Josep Gayà <josepg@limit.es>
2020-07-28 03:18:47 +0200

    Millores al formulari de marcatges

--- cecocloud-module/cecocloud-module-marc/cecocloud-module-marc-front/src/main/resources/src/app/modules/marc/pages/marcatges/marcatges-form.component.ts
+++ cecocloud-module/cecocloud-module-marc/cecocloud-module-marc-front/src/main/resources/src/app/modules/marc/pages/marcatges/marcatges-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BngFormBaseComponent } from 'base-angular';
+import { FormGroup } from '@angular/forms';
+import { BngFormBaseComponent, BngFormBaseField, BngRestapiProfile } from 'base-angular';
+import moment from 'moment';
 
 import { MarcatgesService } from './marcatges.service';
 //import { OperarisEmpresesService } from './operaris-empreses.service';
@@ -11,6 +13,8 @@ import { MarcatgesService } from './marcatges.service';
 	bng-form-mant
 	[config]="formConfig"
 	[restapiService]="marcatgesService"
+	(formGroupChange)="onFormGroupChange($event)"
+	(inputFieldsChange)="onInputFieldsChange($event)"
 	(resourceLoad)="onResourceLoad($event)">
 	<div style="display:flex">
 		<bng-custom-field name="operariEmpresa" style="width:50%;padding-right:2em"></bng-custom-field>
@@ -33,13 +37,31 @@ export class MarcatgesFormComponent extends BngFormBaseComponent implements OnIn
 	longitud: number;
 	latitud: number;
 	showMap: boolean;
+	formGroup: FormGroup;
+	dataField: BngFormBaseField;
+	eachSecondIntervalId: any;
+	dateShowLinkIcon: boolean;
+	dateLinkedWithCurrentTime: boolean;
 
 	ngOnInit() {
-		/*if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				console.log('>>> position', position.coords.latitude, position.coords.longitude);
-			});
-		}*/
+		this.eachSecondIntervalId = setInterval(() => {
+			if (this.dateLinkedWithCurrentTime) {
+				this.formGroup.get('data').setValue(moment());
+			}
+		}, 1000);
+	}
+
+	onFormGroupChange(formGroup: FormGroup) {
+		this.formGroup = formGroup;
+	}
+
+	onInputFieldsChange(inputFields: BngFormBaseField[]) {
+		this.dataField = inputFields.find((inputField: BngFormBaseField) => {
+			return inputField.fieldName == 'data';
+		});
+		if (this.dataField) {
+			this.dataField.readOnlyState = this.dateLinkedWithCurrentTime;
+		}
 	}
 
 	onResourceLoad(resource: any) {
@@ -48,27 +70,32 @@ export class MarcatgesFormComponent extends BngFormBaseComponent implements OnIn
 			this.latitud = resource.latitud;
 			this.showMap = true;
 		}
+		if (this.isAdmin) {
+			this.dateShowLinkIcon = true;
+		}
+		if (!this.id) {
+			this.switchDateLinked();
+		}
+	}
+
+	onDatetimeLinkButtonClick() {
+		this.switchDateLinked();
+	}
+
+	switchDateLinked() {
+		this.dateLinkedWithCurrentTime = !this.dateLinkedWithCurrentTime;
+		if (this.dataField) {
+			this.dataField.readOnlyState = this.dateLinkedWithCurrentTime;
+		}
 	}
 
 	constructor(
 		activatedRoute: ActivatedRoute,
-		public marcatgesService: MarcatgesService/*,
-		operarisEmpresesService: OperarisEmpresesService*/) {
+		public marcatgesService: MarcatgesService) {
 		super(activatedRoute);
-		/*marcatgesService.whenReady().subscribe((marcatgesProfile: BngRestapiProfile) => {
+		marcatgesService.whenReady().subscribe((marcatgesProfile: BngRestapiProfile) => {
 			this.isAdmin = marcatgesProfile.resource.hasAdminPermission;
-			operarisEmpresesService.whenReady().subscribe(() => {
-				if (!this.isAdmin) {
-					operarisEmpresesService.getHttpClient().get(operarisEmpresesService.getApiBaseUrl() + '/current').subscribe((operariEmpresa: any) => {
-						console.log('>>> Current no admin', operariEmpresa);
-					});
-				} else {
-					operarisEmpresesService.getHttpClient().get(operarisEmpresesService.getApiBaseUrl() + '/current').subscribe((operariEmpresa: any) => {
-						console.log('>>> Current admin', operariEmpresa);
-					});
-				}
-			});
-		});*/
+		});
 		this.formConfig.readOnlyStateEnabled = false;
 	}
 
